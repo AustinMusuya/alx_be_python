4fd535b330d87fe7a3ae3db1a3524ce42c7f0cfd
austinmusuya <amusuya57@gmail.com>
2024-09-25 16:29:04 +0300

    modified code in arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -33,7 +33,7 @@ def perform_operation(num1, num2, operation):
         case "divide":
             if num1 == 0 or num2 == 0:
                 return("cannot divide by zero")
-            else:
+            elif num1 !=0 or num2 != 0:
                 result = num1 / num2
 
     return(result)
