1f0628906f88ca93dd430ac3d8e3e598373877d2
austinmusuya <amusuya57@gmail.com>
2024-10-02 16:43:37 +0300

    configure code in bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -82,7 +82,7 @@ enhancing your understanding of Python programming.
 """
 
 
-class BankAccount():
+class BankAccount:
     def __init__(self, account_balance=0):
         self.account_balance = account_balance
 
