e7d56b2fea5ab08f0347d3b76299a40f2eebc1db
austinmusuya <amusuya57@gmail.com>
2024-10-02 16:49:40 +0300

    configure code in bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -97,4 +97,4 @@ class BankAccount:
         return False
 
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
