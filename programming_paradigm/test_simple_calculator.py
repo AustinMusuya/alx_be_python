e3791a8796d7eb27a6e3126b3e4b669e193c1496
austinmusuya <amusuya57@gmail.com>
2024-10-03 10:15:37 +0300

    configure code in test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -94,18 +94,18 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(-1, 1), 0)
         # Add more assertions to thoroughly test the add method.
 
-    def test_subtract(self):
+    def test_subtraction(self):
         # Test the subtraction method.
         self.assertEqual(self.calc.subtract(2, 3), -1)
         self.assertEqual(self.calc.subtract(-1, 1), -2)
         self.assertEqual(self.calc.subtract(4, 2), 2)
 
-    def test_multiply(self):
+    def test_multiplication(self):
         # Test the multiplication method.
         self.assertEqual(self.calc.multiply(2, 3), 6)
         self.assertEqual(self.calc.multiply(-2, 3), -6)
 
-    def test_divide(self):
+    def test_division(self):
         # Test the division method.
         self.assertEqual(self.calc.divide(10, 5), 2)
         self.assertEqual(self.calc.divide(5, 0), None)
